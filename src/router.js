2cd329f8d31c6a9cf4ad83b4c1de6df3ca1baee6
FEZIRO <960838942@qq.com>
2019-08-10 13:58:50 +0800

    change router mode

--- src/router.js
+++ src/router.js
@@ -3,11 +3,10 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
+      path: '*',
       redirect:{
         path:'/all'
       }
